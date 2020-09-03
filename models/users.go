126937f86235681d65f267744943a89705a2ba69
alvinamartya <alvinamartya1@gmial.com>
2020-09-03 19:28:18 +0700

    update user model

--- models/users.go
+++ models/users.go
@@ -1,9 +1,13 @@
 package models
 
 import (
+	"github.com/dgrijalva/jwt-go"
 	"github.com/go-gorm/gorm"
 	"golang.org/x/crypto/bcrypt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 type User struct {
@@ -11,6 +15,12 @@ type User struct {
 	Id       uint   `json:"id" gorm:"primaryKey;autoIncrement:true"`
 	Username string `json:"username"`
 	Password string `json:"password"`
+	Name     string `json:"name"`
+}
+
+type ResponseUser struct {
+	Id       uint   `json:"id" gorm:"primaryKey;autoIncrement:true"`
+	Username string `json:"username"`
 	Name     string `json:"name"`
 }
 
@@ -62,21 +72,123 @@ func (u *User) Create() (map[string]interface{}, int) {
 	u.Password = string(hashedPassword)
 
 	// create new user
-	GetDB().Create(u)
+	errNewUser := GetDB().Create(u).Error
+	if errNewUser != nil {
+		return map[string]interface{}{
+			"error": err,
+		}, http.StatusInternalServerError
+	}
 
+	// user is exists
 	if u.ID <= 0 {
 		return map[string]interface{}{
 			"error": "Register account is failed",
 		}, http.StatusBadRequest
 	}
 
+	// create auth token
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["exp"] = time.Now().Add(time.Hour * 7 * 24).Unix()
+	tokenString, errTokenString := token.SignedString([]byte(os.Getenv("token_password")))
+	if errTokenString != nil {
+		return map[string]interface{}{
+			"error": errTokenString,
+		}, http.StatusInternalServerError
+	}
 
+	// auth model
+	auth := Authentication{
+		UserId:  u.Id,
+		Token:   tokenString,
+		Expired: time.Now().Add(time.Hour * 7 * 24),
+	}
+
+	// user response model
+	newUser := ResponseUser{
+		Id:       u.Id,
+		Username: u.Username,
+		Name:     u.Name,
+	}
+
+	return map[string]interface{}{
+		"user": newUser,
+		"auth": auth,
+	}, http.StatusCreated
 }
 
-func Login(username, password string) map[string]interface{} {
+func Login(username, password string) (map[string]interface{}, int) {
+	u := &User{}
+	err := GetDB().Table("users").Where("username = ?", username).First(u).Error
+	if err != nil {
+		log.Println(err)
+		return map[string]interface{}{
+			"error": err,
+		}, http.StatusInternalServerError
+	}
 
+	if u.Username == "" {
+		return map[string]interface{}{
+			"error": "user not found",
+		}, http.StatusBadRequest
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+		return map[string]interface{}{
+			"error": "Invalid password",
+		}, http.StatusBadRequest
+	}
+
+	// create auth token
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["exp"] = time.Now().Add(time.Hour * 7 * 24).Unix()
+	tokenString, errTokenString := token.SignedString([]byte(os.Getenv("token_password")))
+	if errTokenString != nil {
+		return map[string]interface{}{
+			"error": errTokenString,
+		}, http.StatusInternalServerError
+	}
+
+	// auth model
+	auth := Authentication{
+		UserId:  u.Id,
+		Token:   tokenString,
+		Expired: time.Now().Add(time.Hour * 7 * 24),
+	}
+
+	// user response model
+	newUser := ResponseUser{
+		Username: u.Username,
+		Name:     u.Name,
+		Id:       u.Id,
+	}
+
+	return map[string]interface{}{
+		"user": newUser,
+		"auth": auth,
+	}, http.StatusOK
 }
 
-func GetUser(id int) map[string]interface{} {
+func GetUserById(id int) (map[string]interface{}, int) {
+	u := &User{}
+	err := GetDB().Table("users").Where("id = ?", id).First(u).Error
+	if err != nil {
+		log.Println(err)
+		return map[string]interface{}{
+			"error": "u not found",
+		}, http.StatusBadRequest
+	} else {
+		newUser := ResponseUser{
+			Id:       u.Id,
+			Name:     u.Name,
+			Username: u.Username,
+		}
+
+		return map[string]interface{}{
+			"user": newUser,
+		}, http.StatusOK
+	}
 
 }
